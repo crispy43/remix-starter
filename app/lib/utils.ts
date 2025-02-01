75a2da31c7714b8030db0ba29c1b5f12762834fd
jin <crispy4343@gmail.com>
2025-02-01 09:30:38 +0900

    replaceT 치환 템플릿 2중첩 중괄호로 변경

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -6,11 +6,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 // * localize 템플릿 문자열 치환
-export const replaceT = (
-  template: string,
-  params: Record<string, string | number | boolean>,
-) => {
-  return template.replace(/{(\w+)}/g, (_, key) => {
-    return key in params ? String(params[key]) : `{${key}}`;
-  });
+export const replaceT = (template: string, params: Record<string, string>) => {
+  return template.replace(/{{(.*?)}}/g, (_, key) => params[key.trim()] ?? key);
 };
